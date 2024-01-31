0371c543cf403b29066303371e178d27d3b82169
Reed <reedjmerrill@gmail.com>
2024-01-31 12:49:50 -0700

    sampling procedure output log (json)

--- sample.py
+++ sample.py
@@ -23,7 +23,7 @@ def setup_access():
     return instance
 
 
-def get_posts(subreddit_name, time_period, n_submissions):
+def get_posts(reddit, subreddit_name, time_period, n_submissions):
     """Takes the name of a subreddit, a time period, and the desired number of submissions
     and returns a list of the URLs of that subreddit's top posts.
     
@@ -39,7 +39,7 @@ def get_posts(subreddit_name, time_period, n_submissions):
     return [submission.id for submission in submission_generator]
     
 
-def get_post_comments_ids(submission_id):
+def get_post_comments_ids(reddit, submission_id):
     """Takes a list of submission URLs and returns a (flattened) list of all its comments.
     """
     # create a post instance
@@ -56,22 +56,50 @@ def main():
     time_period = 'year'
     n_submissions = 10
 
-    out = {}
+    seed_to_posts = {}
+    post_to_comments = {}
+    comment_to_user = {}
+    users = {'users': []}
 
+    # iterate through the seed subreddits, getting a list of the IDs of top posts from each
     for seed in SEED_SUBREDDITS:
 
-        out['subreddit'] = seed
+        posts = get_posts(reddit=reddit, subreddit_name=seed, time_period=time_period, n_submissions=n_submissions)
 
-        post_id_list = get_posts(subreddit_name=seed, time_period=time_period, n_submissions=n_submissions)
+        # initialize the output dict and add a key "seed" with items being post IDs
+        seed_to_posts.update({seed: posts})
         
-        for post in post_id_list:
-            comments = get_post_comments_ids(post)
+        # iterate through the posts returned from the seed, retreiving their comment IDs
+        for post in posts:
 
-             = []
+            comments = get_post_comments_ids(reddit=reddit, submission_id=post)
+
+            post_to_comments.update({post: comment})
+            
+            # iterate through the comments, retreiving each one's author
             for comment in comments:
-                users.append(reddit.comment(comment).author)
+
+                # retreived the author of each comment
+                user = reddit.comment(comment).author
                 
+                # add the new comment/user pair to the dict
+                comment_to_user.update({comment: user})
+                
+                users['users'].append(user)
     
+    output_dict = {
+        'seed_to_posts': seed_to_posts,
+        'post_to_comments': post_to_comments,
+        'comment_to_user': comment_to_user
+    }
+
+    # output sampling procedure
+    with open("sampling-prodecure.json", "w") as outfile: 
+        json.dump(output_dict, outfile)
+    
+    # output users dict
+    with open("user-sample.json", "w") as outfile: 
+        json.dump(users, outfile)
 
 if __name__ == "__main__":
     main()
