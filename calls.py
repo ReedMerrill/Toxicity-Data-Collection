477e0895ffc61c557ab010073feab85902b0c22f
Reed <reedjmerrill@gmail.com>
2024-01-31 13:06:32 -0700

    made sample.py just be a job, and moved helpers to calls.py

---
+++ calls.py
@@ -0,0 +1,57 @@
+import praw
+
+# credentials
+CLIENT_ID = "gB_we0e6sW9IK_Xjrw1szQ"
+CLIENT_SECRET = "6rDRP5Hfu6SykadFTDZ8BMi4zSQNUg"
+PASSWORD = "CmhtIf!rc-6ZmoJ"
+USER_AGENT = "Reed's politics scraper v1.0.0 (u/bewchacca-lacca)"
+USERNAME = "bewchacca-lacca"
+
+def setup_access():
+    """Create an instance for API access"""
+    instance = praw.Reddit(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        password=PASSWORD,
+        user_agent=USER_AGENT,
+        username=USERNAME)
+    return instance
+
+
+def get_top_posts(reddit, subreddit_name, time_period, n_submissions):
+    """Takes the name of a subreddit, a time period, and the desired number of submissions
+    and returns a list of the URLs of that subreddit's top posts.
+    
+    nb this is a separate function because doing so retains a list of the submissions, and parsing
+    each level of the user extraction process into its own function avoids messy function outputs
+    and is more efficient.
+    Returns: list of top post URLs
+    """
+    
+    # create the generator
+    submission_generator = reddit.subreddit(subreddit_name).top(time_filter=time_period, limit=n_submissions)
+    # return generator outputs as a list
+    return [submission.id for submission in submission_generator]
+    
+
+def get_post_comments_ids(reddit, submission_id):
+    """Takes a submission ID and returns a (flattened) list of all its comments.
+    """
+    # create a post instance
+    comments = reddit.submission(submission_id).comments
+    # replace_more() updates the comment forest by resolving instances of MoreComments
+    comments.replace_more()
+    # list flattens the comment forest to a simple list of comments, extracting all comment replies
+    return [comment.id for comment in comments.list()]
+
+    # retreived the author of each comment
+def get_comment_author(reddit, comment_id):
+    """Takes a comment ID and return that comment's author.
+    """
+    
+    return reddit.comment(comment_id).author
+
+def get_comment_metadata(reddit, comment_id):
+    """TODO: get all the comment metadata needed for the main analysis"""
+
+    return None
--- sample.py
+++ sample.py
@@ -1,57 +1,13 @@
-import praw
+import calls
 import json
-
-# credentials
-CLIENT_ID = "gB_we0e6sW9IK_Xjrw1szQ"
-CLIENT_SECRET = "6rDRP5Hfu6SykadFTDZ8BMi4zSQNUg"
-PASSWORD = "CmhtIf!rc-6ZmoJ"
-USER_AGENT = "Reed's politics scraper v1.0.0 (u/bewchacca-lacca)"
-USERNAME = "bewchacca-lacca"
 
 # single sub for testing
 seeds_dict = json.load(open('seed-subreddits.json'))
 SEED_SUBREDDITS = seeds_dict['all']
 
-def setup_access():
-    """Create an instance for API access"""
-    instance = praw.Reddit(
-        client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET,
-        password=PASSWORD,
-        user_agent=USER_AGENT,
-        username=USERNAME)
-    return instance
-
-
-def get_posts(reddit, subreddit_name, time_period, n_submissions):
-    """Takes the name of a subreddit, a time period, and the desired number of submissions
-    and returns a list of the URLs of that subreddit's top posts.
-    
-    nb this is a separate function because doing so retains a list of the submissions, and parsing
-    each level of the user extraction process into its own function avoids messy function outputs
-    and is more efficient.
-    Returns: list of top post URLs
-    """
-    
-    # create the generator
-    submission_generator = reddit.subreddit(subreddit_name).top(time_filter=time_period, limit=n_submissions)
-    # return generator outputs as a list
-    return [submission.id for submission in submission_generator]
-    
-
-def get_post_comments_ids(reddit, submission_id):
-    """Takes a list of submission URLs and returns a (flattened) list of all its comments.
-    """
-    # create a post instance
-    comments = reddit.submission(submission_id).comments
-    # replace_more() updates the comment forest by resolving instances of MoreComments
-    comments.replace_more()
-    # list flattens the comment forest to a simple list of comments, extracting all comment replies
-    return [comment.id for comment in comments.list()]
-
 def main():
 
-    reddit = setup_access()
+    reddit = calls.setup_access()
 
     time_period = 'year'
     n_submissions = 10
@@ -64,23 +20,22 @@ def main():
     # iterate through the seed subreddits, getting a list of the IDs of top posts from each
     for seed in SEED_SUBREDDITS:
 
-        posts = get_posts(reddit=reddit, subreddit_name=seed, time_period=time_period, n_submissions=n_submissions)
+        posts = calls.get_top_posts(reddit=reddit, subreddit_name=seed, time_period=time_period, n_submissions=n_submissions)
 
-        # initialize the output dict and add a key "seed" with items being post IDs
+        # add a key "seed" with the post IDs as items
         seed_to_posts.update({seed: posts})
         
         # iterate through the posts returned from the seed, retreiving their comment IDs
         for post in posts:
 
-            comments = get_post_comments_ids(reddit=reddit, submission_id=post)
+            comments = calls.get_post_comments_ids(reddit=reddit, submission_id=post)
 
             post_to_comments.update({post: comment})
             
             # iterate through the comments, retreiving each one's author
             for comment in comments:
 
-                # retreived the author of each comment
-                user = reddit.comment(comment).author
+                user = calls.get_comment_author(reddit=reddit, comment_id=comment)
                 
                 # add the new comment/user pair to the dict
                 comment_to_user.update({comment: user})
