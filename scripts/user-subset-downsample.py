313785bbc077dfb74c8b8b01a478e847963a1fed
Reed <reedjmerrill@gmail.com>
2024-02-21 13:41:09 -0700

    removed column subsetting

--- scripts/user-subset-downsample.py
+++ scripts/user-subset-downsample.py
@@ -6,7 +6,7 @@ PROJECT_PATH = "/home/reed/Projects/learned-toxicity-reddit/reddit-api/"
 INPUT_PATH = (
     f"{PROJECT_PATH}data/comments/sample-subset/20pct-users-subset_comments.csv"
 )
-OUTPUT_PATH = f"{PROJECT_PATH}data/comments/"
+OUTPUT_PATH = f"{PROJECT_PATH}data/comments/sample-subset/"
 
 import pandas as pd
 import random
@@ -16,9 +16,7 @@ comments = pd.read_csv(INPUT_PATH)
 # downsample by 50%
 comments_subset = comments.sample(n=round(comments.shape[0] * 0.55), random_state=1)
 
-print(f"Original N: {comments.shape[0]}, Subset Length: {comments_subset.shape[0]}")
+# print(f"Original N: {comments.shape[0]}, Subset Length: {comments.shape[0]}")
 
-out = pd.DataFrame(comments_subset, columns=["users"])
-
-with open(f"{OUTPUT_PATH}10pct-users-subset_comments", "w") as file:
-    out.to_csv(file, index=False)
+with open(f"{OUTPUT_PATH}10pct-users-subset_comments.csv", "w") as file:
+    comments_subset.to_csv(file, index=False)
