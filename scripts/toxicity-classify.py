8ff7b8707337dcdf953a33ca7d9a5df9e3dcb1dd
Reed <reedjmerrill@gmail.com>
2024-02-21 13:41:24 -0700

    WIP batching w/ comment IDs

--- scripts/toxicity-classify.py
+++ scripts/toxicity-classify.py
@@ -15,7 +15,7 @@ from transformers import (
 import utils
 
 PROJECT_PATH = "/home/reed/Projects/learned-toxicity-reddit/reddit-api/"
-INPUT_PATH = f"{PROJECT_PATH}data/comments/20pct-users-subset_comments.csv"
+INPUT_PATH = f"{PROJECT_PATH}data/comments/10pct-users-subset_comments.csv"
 OUTPUT_PATH = f"{PROJECT_PATH}data/comments/toxicity-classified-comments.csv"
 
 model_name = "s-nlp/roberta_toxicity_classifier"
@@ -28,45 +28,52 @@ classifier = TextClassificationPipeline(
 # clean up before rerun
 if os.path.exists(OUTPUT_PATH):
     os.remove(OUTPUT_PATH)
-    print(f"A pre-existing output file has been successfully deleted.")
+    print(f"A pre-existing output file has been deleted.")
 
 
-def batch_list(comments_list, batch_size):
-    # create batches
-    return [
-        comments_list[i : i + batch_size]
-        for i in range(0, len(comments_list), batch_size)
-    ]
+def batch_df(comments_df, batch_size):
+    """Takes a DF and returns a list of DFs that are slices of the original,
+    each with batch_size number of rows."""
+    n_rows = comments_df.shape[0]
+    batches = []
+    for i in range(0, n_rows, batch_size):
+        batch = [comments_df.iloc[i : i + batch_size, :]]
+        batches.append(batch)
+
+    return batches
 
 
 def main():
     data = pd.read_csv(INPUT_PATH)
-    comments = data["comment_id", "text"]
+    print("++++++++++++++++++")
+    print(data.columns)
     data["text"] = data["text"].map(utils.clean_comment)
     data["text"] = data["text"].map(utils.remove_emojis)
+    comments_df = data["comment_id", "text"]
     start = time.time()
 
-    for i in range(0, data.shape[0], 64):
-        # index the next batch of text (row, col)
-        batch_text = data.iloc[i : i + 64, 1]
-        # get batches of comment ids
-        batch_comment_ids = data.iloc[i : i + 64, 0]
-        # do the batching
-        out = classifier(batch_text, truncation=True, max_length=512)
+    batches_list = batch_df(comments_df=comments_df, batch_size=4)
 
+    # loop over the batches in batches_list
+    for batch in batches_list:
+
+        comment_ids = batch["comment_id"]
+        comments_text = list(batch["text"])
+        out = classifier(comments_text, truncation=True, max_length=512)
+        print(out)
         # streaming output to CSV
-        # Extract labels and scores into separate lists
+        # Extract labels and scores into separate lists. Need lists to make df
         label = [o["label"] for o in out]
         score = [o["score"] for o in out]
 
         # define header and attach output
         data_batch = pd.DataFrame(
             {
+                "comment_id": comment_id,
                 "toxicity_label": label,
                 "toxicity_score": score,
             }
         )
-        # maybe just do a join of the ids back onto the toxicity labels now?
 
         file_exists = True if os.path.exists(OUTPUT_PATH) else False
         if file_exists is False:
@@ -77,12 +84,12 @@ def main():
                 data_batch.to_csv(file, index=False, header=False)
 
         # logging
-        print(f"Comments labelled: {(i + 1 * 64)}")
-        estimate = utils.estimate_time_remaining(i, len(comment_batch_list), start)
+        print(f"Comments labelled: {(index + 1 * 64)}")
+        estimate = utils.estimate_time_remaining(index, len(batches_list), start)
         print(f"Time remaining: ~{estimate} hours")
 
-    # log time elapsed
-    print(f"Time Elapsed: {time.time() - start}")
+        # log time elapsed
+        print(f"Time Elapsed: {time.time() - start}")
 
 
 if __name__ == "__main__":
