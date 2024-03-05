86b7c3d53380a3e64d77957d181c53e058d4541a
Reed <reedjmerrill@gmail.com>
2024-03-05 15:21:22 -0700

    fixed df col types

--- scripts/user-seed.py
+++ scripts/user-seed.py
@@ -3,10 +3,13 @@
 import json
 import pandas as pd
 
+DATA_DIR = (
+    "/home/reed/Projects/learned-toxicity-reddit/toxicity-data-collection/data/sample/"
+)
 # load data
 sampling_frame_dict = json.load(
     open(
-        "/home/reed/Projects/learned-toxicity-reddit/toxicity-data-collection/data/sample/sampling-procedure.json"
+        DATA_DIR + "sampling-procedure.json",
     )
 )
 
@@ -27,7 +30,7 @@ post_to_comments_df = pd.DataFrame({"post": [], "comment": []})
 for key in post_to_comments_dict:
     key_col = [key] * len(post_to_comments_dict[key])
     df = pd.DataFrame([key_col, post_to_comments_dict[key]]).T
-    df.columns = ["post", "comments"]
+    df.columns = ["post", "comment"]
     post_to_comments_df = pd.concat([post_to_comments_df, df], axis=0)
 
 # comments to users
@@ -38,28 +41,38 @@ user_ids = [sampling_frame_dict["comment_to_user"][comment] for comment in comme
 # sanity checks
 # are the two lists the same length?
 assert len(comment_ids) == len(user_ids)
-# does it have the right number of users?
-print(len(list(set(user_ids))))  # 10783
+# are the comment ids unique?
+assert len(list(set(comment_ids))) == len(comment_ids)
 
 # create dataframe
 comments_to_users_df = pd.DataFrame({"comment": comment_ids, "user": user_ids})
 # keep only unique rows
 comments_to_users_df = comments_to_users_df.drop_duplicates("user")
 
+
 # verify that the number of unique users is the same in the final dataframe
 assert len(list(set(user_ids))) == comments_to_users_df.shape[0]
 
-# ==============================================================================
+# =============================================================================
 # merge dataframes
-# ==============================================================================
+# =============================================================================
+
+# type conversions
+comments_to_users_df["comment"] = comments_to_users_df["comment"].astype(str)
+post_to_comments_df["comment"] = post_to_comments_df["comment"].astype(str)
+seed_to_posts_df["post"] = seed_to_posts_df["post"].astype(str)
+
 # merge comments to users
 comments_to_posts_df = comments_to_users_df.merge(
-    post_to_comments_df, how="left", on="comment"
+    post_to_comments_df, on="comment", how="left", validate="m:1"
 )
 
 # merge posts to comments
-comments_to_seeds_df = comments_to_posts_df.merge(
-    seed_to_posts_df, how="left", on="post"
+users_to_seeds_df = comments_to_posts_df.merge(
+    seed_to_posts_df, on="post", how="left", validate="m:1"
 )
 
-print(comments_to_seeds_df.shape[0])
+assert users_to_seeds_df.shape[0] == len(list(set(user_ids)))
+
+with open(DATA_DIR + "user-seed-lookup.csv", "w") as f:
+    users_to_seeds_df.to_csv(f, index=False)
